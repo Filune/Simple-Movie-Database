cfa7adc6a5dfa9834e8b8e780be2ecb9287a3c19
filune <3577394+Filune@users.noreply.github.com>
2022-04-11 22:25:05 +0200

    Revision 1.0
    Signed-off-by: filune <3577394+Filune@users.noreply.github.com>

---
+++ Launcher.java
@@ -0,0 +1,18 @@
+/*
+ * Författare: Filune
+ * Datum: 2020-07-20
+ * Uppgift: Laboration 5
+ */
+
+import java.io.FileNotFoundException;
+
+/*
+ * Launcher har enbart en uppgift, vilket är att starta upp filmdatabasen.
+ */
+public class Launcher {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		new Database();
+	}
+	
+}
